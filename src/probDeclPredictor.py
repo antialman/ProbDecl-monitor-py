ff11ec049db801adb99c9f14134c4dc7a8b9f606
antialman <anti.alman@gmail.com>
2025-02-19 18:47:31 +0200

    Updated the ProbDeclarePredictor filename

--- src/probDeclPredictor.py
+++ src/probDeclPredictor.py
@@ -233,18 +233,3 @@ class ProbDeclarePredictor:
         print()
 
         return nextEventScores
-
-
-
-
-
-modelPath = os.path.join("input", "model_01_probDecl.txt")
-prefix = ["b", "x", "b", "a", "a", "b"]
-
-probDeclarePredictor = ProbDeclarePredictor()
-probDeclarePredictor.loadProbDeclModel(modelPath)
-result = probDeclarePredictor.processPrefix(prefix)
-
-print("Final ranking:")
-for event, score in sorted(result.items(), key=operator.itemgetter(1), reverse=True):
-    print("    " + str(event) + ": " + str(score)) #Using str(event) because result contains the keyword False for stopping the execution and the keyword True for any unknown activity
